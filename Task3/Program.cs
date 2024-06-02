b832953727170d2eeb6a38c615fadb649269a3a8
Rustam <rustamos@list.ru>
2024-06-02 17:51:01 +0500

    Task3 complete

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+   static void Main(string[] args)
+   {
+       int[] array = { 1, 2, 5, 0, 10, 34 };
+       PrintArrayReversed(array, array.Length - 1);
+   }
+   static void PrintArrayReversed(int[] array, int index)
+   {
+       if (index >= 0)
+       {
+           Console.Write(array[index] + " ");
+           PrintArrayReversed(array, index - 1);
+       }
+   }
+}
