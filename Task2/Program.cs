6c47473745c5ce403805ce613044b7093b0a0b4d
Rustam <rustamos@list.ru>
2024-06-02 17:42:20 +0500

    Task2 complete

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) {
+        return akkermanFunction(m - 1, 1);
+    }
+    else 
+    {
+        return akkermanFunction( m - 1 , akkermanFunction(m, n - 1));
+    }
+}
+
+Console.Write($"Число по функции Аккермана =  {akkermanFunction(m, n)} ");
