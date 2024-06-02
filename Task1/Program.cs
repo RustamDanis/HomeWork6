047a7c17fc1f42670603c9f4bbc3b476136a8e33
Rustam <rustamos@list.ru>
2024-06-02 16:51:24 +0500

    Task1 complete

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Введите число М: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    PrintNumInrange(M, N);
+}
+
+static void PrintNumInrange(int start, int end)
+{
+    if(start <= end)    
+    {
+        Console.Write(start);
+        if (start < end)
+        Console.Write(", ");
+        PrintNumInrange(start + 1, end);
+        
+    }
+}
+}
